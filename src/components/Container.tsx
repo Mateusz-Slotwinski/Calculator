57713f24aa942febcef4842ca3bf58840e1c631e
Mateusz <mateuszslotwinski16@gmail.com>
2021-08-22 22:24:16 +0200

    .

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -13,8 +13,8 @@ function Container() {
 const Background = styled.div`
 width:100vw;
 height:100vh;
-background: rgb(224,97,45);
-background: linear-gradient(117deg, rgba(224,97,45,1) 0%, rgba(221,165,42,1) 42%, rgba(0,212,255,1) 100%);
+background: rgb(9,24,31);
+background: linear-gradient(145deg, rgba(9,24,31,1) 0%, rgba(9,54,64,1) 35%, rgba(13,65,47,1) 100%);
 display:flex;
 justify-content:center;
 align-items:center;`
